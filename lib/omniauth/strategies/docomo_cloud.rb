ad58bce0d35f1897f3c1bd44710eea68b5c4a9b0
Takahiro HAMAGUCHI <tk.hamaguchi@gmail.com>
2014-02-10 14:18:31 +0900

    update authorize_url.

--- lib/omniauth/strategies/docomo_cloud.rb
+++ lib/omniauth/strategies/docomo_cloud.rb
@@ -7,9 +7,9 @@ module OmniAuth
       option :name, 'docomo_cloud'
 
       option :client_options, {
-        site:           'https://api.smt.docomo.ne.jp',
-        authorize_path: '/api/login',
-        token_url:      '/1/access_token'
+        site:          'https://api.smt.docomo.ne.jp',
+        authorize_url: '/api/login',
+        token_url:     '/1/access_token'
       }
 
       uid{ access_token.to_s }
